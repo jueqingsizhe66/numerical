ae99b44be0a75dda661611e773a6b59cae472acb
John Tramm <john.tramm@gmail.com>
2012-02-07 23:27:34 -0600

    added settings.cpp

---
+++ settings.cpp
@@ -0,0 +1,23 @@
+#include "header.h"
+
+void Settings::calculate_deltas(void)
+{
+	dx = l_x / p_x;
+	dy = l_y / p_y;
+	dz = l_z / p_z;
+};
+
+void Settings::print(void)
+{
+	cout << "Settings Summary" << endl;
+	cout << "----------------" << endl;
+	cout << "	Domain Boundaries (l_x, l_y, l_z): (";
+	cout << l_x << " " << l_y << " " << l_z << ")" << endl;
+	cout << "	Number of Partitions (p_x, p_y, p_z): (";
+	cout << p_x << " " << p_y << " " << p_z << ")" << endl;
+	cout << "	Deltas (dx, dy, dz): (";
+	cout << dx << " " << dy << " " << dz << ")" << endl;
+	cout << "	Alpha: " << alpha << endl;
+	cout << "	Timestep (dt): " << dt << endl;
+	cout << "	Number of Iterations:" << nsteps << endl;
+}
