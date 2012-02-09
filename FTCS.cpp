16e44ddc618b4e25a51db2376879872c86ed1d55
John Tramm <john.tramm@gmail.com>
2012-02-08 19:04:49 -0600

    formatting

--- FTCS.cpp
+++ FTCS.cpp
@@ -23,14 +23,16 @@ Domain FTCS( Domain T )
 			for( int j = 1; j <= T.settings.p_y; j++)
 				for( int k = 1; k <= T.settings.p_z; k++ )
 					Tnew.m[i][j][k] = T.m[i][j][k] + T.settings.alpha * T.settings.dt *
-				  	                (
-					                  	( T.m[i+1][j][k] - 2*T.m[i][j][k] + T.m[i-1][j][k] )
-					                      / pow( T.settings.dx, 2 ) +
-						                  ( T.m[i][j+1][k] - 2*T.m[i][j][k] + T.m[i][j-1][k] )
-					                      / pow( T.settings.dy, 2 ) +
-					                  	( T.m[i][j][k+1] - 2*T.m[i][j][k] + T.m[i][j][k-1] )
-					                      / pow( T.settings.dz, 2 )
-					                  );
+					                (
+					                	( T.m[i+1][j][k] - 2*T.m[i][j][k] + T.m[i-1][j][k] )
+					                		/ pow( T.settings.dx, 2 ) +
+					                	( T.m[i][j+1][k] - 2*T.m[i][j][k] + T.m[i][j-1][k] )
+					                		/ pow( T.settings.dy, 2 ) +
+					                	( T.m[i][j][k+1] - 2*T.m[i][j][k] + T.m[i][j][k-1] )
+					                		/ pow( T.settings.dz, 2 )
+					                )
+					                + sourceTerm( i, j, k );
+
 		T = Tnew;
 	}
 	return T;
