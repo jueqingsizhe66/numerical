42d25b5766ece02f8c7f5877bd7a38d9bf1c8c6a
John Tramm <john.tramm@gmail.com>
2012-02-14 16:24:46 -0600

    CN implementation coming along :)

--- CN.cpp
+++ CN.cpp
@@ -2,37 +2,29 @@
 
 Domain CN( Domain T )
 {
+	double C = T.settings.dt * T.settings.aplha / 2;
+	double diag = 1 + 2*C / pow(T.settings.dx, 2) + 2*C / pow(T.settings.dy,2) + 2*C/pow(T.settings.dz,2);
+	double xdiag = - C / pow(T.settings.dx,2);
+	double ydiag = - C / pow(T.settings.dy,2);
+	double zdiag = - C / pow(T.settings.dz,2);
+
+	// First, we need to load our temperature domain into
+	// a vector format for x.
+	vector<double> x;
+	for( int i = 1; i < (T.settings.p_x + 2) * (T.settings.p_y+2) * (T.settings.p_z+2); i++ )
+	{
+		x.push_back( T.m[i%p_x][(i/p_x)%p_y][((i/p_x)%p_y)%p_z] );
+	}
+	
+	vector<double> xnew;
+
+
 	Domain Tnew = T;
 	Tnew.updateBC(0);
 	T.updateBC(0);
 	for( int n = 0; n < T.settings.nsteps; n++ )
 	{
-		T.updateBC(n);
-		cout << "t = " << n << endl;
-		cout << "T[50][50][50] = " << T.m[50][50][50] << endl;
-		cout << "T[51][50][50] = " << T.m[51][50][50] << endl;
-		cout << "T[49][50][50] = " << T.m[49][50][50] << endl;
-		cout << "T[50][51][50] = " << T.m[50][51][50] << endl;
-		cout << "T[50][49][50] = " << T.m[50][49][50] << endl;
-		cout << "T[50][50][51] = " << T.m[50][50][51] << endl;
-		cout << "T[50][50][49] = " << T.m[50][50][49] << endl;
-		cout << "Edge:         = " << T.m[0][0][0] << endl;
-		cout << "Inside Edge:  = " << T.m[1][1][1] << endl;
-		
-		for( int i = 1; i <= T.settings.p_x; i++ )
-			for( int j = 1; j <= T.settings.p_y; j++)
-				for( int k = 1; k <= T.settings.p_z; k++ )
-					Tnew.m[i][j][k] = T.m[i][j][k] + T.settings.alpha * T.settings.dt *
-					                (
-					                	( T.m[i+1][j][k] - 2*T.m[i][j][k] + T.m[i-1][j][k] )
-					                		/ pow( T.settings.dx, 2 ) +
-					                	( T.m[i][j+1][k] - 2*T.m[i][j][k] + T.m[i][j-1][k] )
-					                		/ pow( T.settings.dy, 2 ) +
-					                	( T.m[i][j][k+1] - 2*T.m[i][j][k] + T.m[i][j][k-1] )
-					                		/ pow( T.settings.dz, 2 )
-					                )
-					                + sourceTerm( i, j, k );
-
+		vector 
 		T = Tnew;
 	}
 	return T;
