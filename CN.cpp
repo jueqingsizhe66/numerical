91e6c4d551abf12b0331cebf3eb5a16ca3f3c6ab
John Tramm <john.tramm@gmail.com>
2012-02-14 23:25:13 -0600

    CN first draft complete

--- CN.cpp
+++ CN.cpp
@@ -2,70 +2,124 @@
 
 Domain CN( Domain T )
 {
-	double C = T.settings.dt * T.settings.aplha / 2;
-	double diag = 1 + 2*C / pow(T.settings.dx, 2) + 2*C / pow(T.settings.dy,2) + 2*C/pow(T.settings.dz,2);
-	double xdiag = - C / pow(T.settings.dx,2);
-	double ydiag = - C / pow(T.settings.dy,2);
-	double zdiag = - C / pow(T.settings.dz,2);
-
-	// First, we need to load our temperature domain into
-	// a vector format for x.
-	vector<double> x;
-	int xs;
-	int ys;
-	int zs;
-	int length = (T.settings.p_x + 2) * (T.settings.p_y+2) * (T.settings.p_z+2);
-	for( int i = 1; i <= length ; i++ )
+	for( int it = 0; it < T.settings.nsteps; it++)
 	{
-		xs = i % p_x;
-		ys = ( i / p_x ) % p_y;
-		zs = ( ( i / p_x ) / p_y ) % p_z;
-		x.push_back( T.m[xs][ys][zs] );
-	}
-	// x has been initialized
+		T.updateBC( it );
+		double C = T.settings.dt * T.settings.aplha / 2;
+		double diag = 1 + 2*C / pow(T.settings.dx, 2) + 2*C / pow(T.settings.dy,2) + 2*C/pow(T.settings.dz,2);
+		double xdiag = - C / pow(T.settings.dx,2);
+		double ydiag = - C / pow(T.settings.dy,2);
+		double zdiag = - C / pow(T.settings.dz,2);
 
-	// Now I need to make A.
-	vector<vector<double> > A;
-	for( int i = 1; i <= length; i++ )
-	{
-		for( int j = 1; j <= length; j++ )
+		// First, we need to load our temperature domain into
+		// a vector format for x.
+		vector<double> x;
+		int xs;
+		int ys;
+		int zs;
+		int length = (T.settings.p_x + 2) * (T.settings.p_y+2) * (T.settings.p_z+2);
+		for( int i = 1; i <= length ; i++ )
 		{
-			if( i == j )
-				A[i][j] = diag;
-			else if( j == i - 7 || j == i + 7 ) // far back z (check on 7)
-				A[i][j] = zdiag;
-			else if( j == i - 3 || j == i + 3 ) // These don't take into account the 'sub-square' pattern effect
-				A[i][j] == ydiag;
-			else if( j == i - 1 || j == i + 1 ) // but I'll add that later...
-				A[i][j] = xdiag;
-			else
-				A[i][j] = 0;
+			xs = i % p_x;
+			ys = ( i / p_x ) % p_y;
+			zs = ( ( i / p_x ) / p_y ) % p_z;
+			x.push_back( T.m[xs][ys][zs] );
 		}
-	}
-	// A has now been created.
-	
-	// Now I need to calculate the inital b.
-	// Each entry is for the RHS of the respective AX=b equation.
-	// I need my notes for this.
+		// x has been initialized
 
-	
-	
-	// b has been initialized.
+		// Now I need to make A.
+		vector<vector<double> > A;
+		for( int i = 1; i <= length; i++ )
+		{
+			for( int j = 1; j <= length; j++ )
+			{
+				if( i == j )
+					A[i][j] = diag;
+				else if( j == i - 9 || j == i + 9 ) // far back z (check on 7)
+					A[i][j] = zdiag;
+				else if( j == i - 3 || j == i + 3 ) // These don't take into account the 'sub-square' pattern effect
+					A[i][j] == ydiag;
+				else if( j == i - 1 || j == i + 1 ) // but I'll add that later...
+					A[i][j] = xdiag;
+				else
+					A[i][j] = 0;
+			}
+		}
+		// A has now been created.
+		
+		// Now I need to calculate the inital b.
+		// Each entry is for the RHS of the respective AX=b equation.
+		// I need my notes for this.
 
-	// Now I need to run gaussian elimination to determine the new x vector.
+		vector<double> b = x; // to initialize it to the right size
+		
+		for( int i = 1; i <= length; i++ )
+		{
+			xs = i % p_x;
+			ys = ( i / p_x ) % p_y;
+			zs = ( ( i / p_x ) / p_y ) % p_z;
+			
+			double term1 = C / pow(T.settings.dx,2) * (T.m[xs+1][ys][zs] - 2*T.m[xs][ys][zs] + T.m[xs-1][ys][zs]);
+			double term2 = C / pow(T.settings.dy,2) * (T.m[xs][ys+1][zs] - 2*T.m[xs][ys][zs] + T.m[xs][ys-1][zs]);
+			double term3 = C / pow(T.settings.dz,2) * (T.m[xs][ys][zs+1] - 2*T.m[xs][ys][zs] + T.m[xs][ys][zs-1]);
+			
+			b[i] = T.m[xs][ys][zs] + term1 + term2 + term3;
+		}
+		// b has been initialized.
 
-	// After elimination, xnew vector must be fed into next iteration.
-
-	vector<double> xnew;
+		// Now I need to run gaussian elimination to determine the new x vector.
 
 
-	Domain Tnew = T;
-	Tnew.updateBC(0);
-	T.updateBC(0);
-	for( int n = 0; n < T.settings.nsteps; n++ )
-	{
-		vector 
-		T = Tnew;
+	// GAUSSIAN ELIMINATION
+
+	//upper-triangulate 
+		double scale;
+		for (int j=1;j<=length;++j)           /* loop over columns */
+			for (int i=j+1;i<=length;++i)      /* loop over rows beneath pivot */
+			{
+				for( int l = i+1; l <= length; l++ ) // loop checks if there's a bigger pivot
+					if( A[i][j] < A[l][j] )      // and swaps rows if there is.
+					{	
+						for( int k = 1; k <= length; k++ )
+						{
+							// swaps rows
+							double tmp = A[i][k];
+							A[i][k] = A[l][k];
+							A[l][k] = tmp;
+						}
+						l = i;
+					}
+				if (A[i][j] != 0)
+				{
+					scale = A[i][j]/A[j][j];  /* zero out based on pivot */
+					for (int k=1;k<=length;++k)
+						A[i][k] = A[i][k] - A[j][k]*scale;
+					b[i] = b[i] - b[j]*scale; /* same for b */
+				}
+			}
+		
+	// Back substitution
+		x[length] = b[length]/A[length][length];
+
+		for (int i=length-1;i>=1;--i){
+			x[i] = b[i];
+			for (j=i+1;j<=length;++j){
+				x[i] -= A[i][j]*x[j];
+			}
+			x[i]/=A[i][i];
+		}
+
+		// After elimination, xnew vector must be fed into next iteration.
+
+		// loads solution vector (x) back into the Domain object
+		for( int i = 1; i <= length ; i++ )
+		{
+			xs = i % p_x;
+			ys = ( i / p_x ) % p_y;
+			zs = ( ( i / p_x ) / p_y ) % p_z;
+			T.m[xs][ys][zs] = x[i];
+		}
+
 	}
 	return T;
 
