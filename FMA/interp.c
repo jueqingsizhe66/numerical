9253a7116e6ec9b343545ebac4ead42c040d06a8
John Tramm <john.tramm@gmail.com>
2012-03-14 18:22:58 -0500

    first rough draft of 2d to 3d conversion done

--- FMA/interp.c
+++ FMA/interp.c
@@ -1,22 +1,27 @@
-void interp(double **uf, double **uc, int nf)
+void interp(double ***uf, double ***uc, int nf)
 /*
   Coarse-to-fine prolongation by bilinear interpolation. nf is the fine-grid dimension. The coarsegrid
   solution is input as uc[1..nc][1..nc], where nc = nf/2 + 1. The fine-grid solution is
   returned in uf[1..nf][1..nf].
 */
 {
-  int ic,iif,jc,jf,nc;
+  int ic,iif,jc,jf,kc,kf,nc;
   nc=nf/2+1;
-  /* Do elements that are copies.*/
-  for (jc=1,jf=1;jc<=nc;jc++,jf+=2) 
-    for (ic=1;ic<=nc;ic++) 
-      uf[2*ic-1][jf]=uc[ic][jc];
-  /* Do odd-numbered columns, interpolating vertically.*/
-  for (jf=1;jf<=nf;jf+=2) 
-    for (iif=2;iif<nf;iif+=2) 
-      uf[iif][jf]=0.5*(uf[iif+1][jf]+uf[iif-1][jf]);
-  /*Do even-numbered columns, interpolating horizontally.*/
-  for (jf=2;jf<nf;jf+=2) 
-    for (iif=1;iif <= nf;iif++) 
-      uf[iif][jf]=0.5*(uf[iif][jf+1]+uf[iif][jf-1]);
+	// my interpolation method basically just does 2-D interpolation for each slice of the cube.
+	// This seems like a reasonable methodology to me.
+	for (kc=1,kf=1;kc<=nc;kc++,kf+=2)
+  {
+		/* Do elements that are copies.*/
+		for (jc=1,jf=1;jc<=nc;jc++,jf+=2) 
+			for (ic=1;ic<=nc;ic++) 
+				uf[2*ic-1][jf][kf]=uc[ic][jc][kc];
+		/* Do odd-numbered columns, interpolating vertically.*/
+		for (jf=1;jf<=nf;jf+=2) 
+			for (iif=2;iif<nf;iif+=2) 
+				uf[iif][jf][kf]=0.5*(uf[iif+1][jf]+uf[iif-1][jf]);
+		/*Do even-numbered columns, interpolating horizontally.*/
+		for (jf=2;jf<nf;jf+=2) 
+			for (iif=1;iif <= nf;iif++) 
+				uf[iif][jf][kf]=0.5*(uf[iif][jf+1]+uf[iif][jf-1]);
+	}
 }
--- FMA/relax.c
+++ FMA/relax.c
@@ -1,4 +1,4 @@
-void relax(double **u, double **rhs, int n)
+void relax(double ***u, double ***rhs, int n)
 /*
   Red-black Gauss-Seidel relaxation for model problem. Updates the current value of the solution
   u[1..n][1..n], using the right-hand side function rhs[1..n][1..n].
@@ -13,12 +13,22 @@ void relax(double **u, double **rhs, int n)
      determine starting row in each column
      for given pass 
   */
-  for (ipass=1;ipass<=2;ipass++,jsw=3-jsw)
-	{ 
-    isw=jsw;
-    for (j=2;j<n;j++,isw=3-isw)
-      /*Gauss-Seidel formula.*/
-      for (i=isw+1;i<n;i+=2) 
-				u[i][j]=0.25*(u[i+1][j]+u[i-1][j]+u[i][j+1]+u[i][j-1]-h2*rhs[i][j]);
-  }
+
+	// My method is to do sweeps through each slice of the cube.
+	int k;
+	for (k=2;k<n;k++)
+		for (ipass=1;ipass<=2;ipass++,jsw=3-jsw)
+		{ 
+			isw=jsw;
+			for (j=2;j<n;j++,isw=3-isw)
+				/*Gauss-Seidel formula.*/
+				for (i=isw+1;i<n;i+=2) 
+					u[i][j][k]=0.166*
+					           (
+										 u[i+1][j][k] + u[i-1][j][k]
+										 + u[i][j+1][k] + u[i][j-1][k] 
+										 + u[i][j][k+1] + u[i][j][k-1]
+										 - h2*rhs[i][j][k]
+										 );
+		}
 }
